ba5c53f6f833206ba397f03ec790d2c7cbb67e32
Chris Maxwell <cmaxwell@ojala.com>
2013-03-08 15:30:42 -0500

    Update application.js and _head.html.haml.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -10,5 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require modernizr-2.6.2.min
 //= require jquery-1.8.3.min
 //= require site
