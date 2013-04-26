4775b36c96f0f3d4d3a3d872552f2500ee1d3826
Chris Maxwell <cmaxwell@ojala.com>
2013-04-26 11:53:36 -0400

    Update jQuery.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require jquery
+//= require jquery_ujs
 //= require modernizr-2.6.2.min
-//= require jquery-1.8.3.min
+//= require jquery-1.9.1.min
 //= require site
