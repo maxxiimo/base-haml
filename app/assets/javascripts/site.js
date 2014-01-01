0d06d00b151b38c0e4c36b2c93b30e585812cc9e
Chris Maxwell <cmaxwell@ojala.com>
2014-01-01 12:12:59 -0500

    Comment out JavaScript, leave in case needed.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,12 @@
+// ===========================
 // Remove Anchor Outline
+// ===========================
 
 // http://stackoverflow.com/questions/1535538/jquery-getting-rid-of-dotted-outline-around-buttons
 // Not recommended: http://css-tricks.com/removing-the-dotted-outline/
 
-$(function() {
-    $('.autoblur').live("click", function(event) {
-        this.blur();
-    });
-});
+// $(function() {
+//     $('.autoblur').live("click", function(event) {
+//         this.blur();
+//     });
+// });
